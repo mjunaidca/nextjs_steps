8523e14c9dd8fd26049b2310500efefe91a98ce5
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-03-20 19:20:25 +0500

    Skim 08 all sub steps

--- beta-13.2/src/app/(apis-invoke)/data-client/page.tsx
+++ beta-13.2/src/app/(apis-invoke)/data-client/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client'
+
+import useSWR from 'swr';
+
+const url = 'https://api.quotable.io/random?tags=technology';
+
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
 export default function page() {
+    const { data, error, isLoading } = useSWR(url, fetcher);
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
     return (
-        <div>page</div>
+        <div>{data.content}</div>
     )
 }
