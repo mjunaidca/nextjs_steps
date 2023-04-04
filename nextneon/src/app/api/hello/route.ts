6d196ee9bde3c72e7f3b19696ac7ffa65098f2a2
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-04 20:45:26 +0500

    step 01_2 working sql configured and completed

--- nextneon/src/app/api/hello/route.ts
+++ nextneon/src/app/api/hello/route.ts
@@ -10,11 +10,43 @@
 //   return new NextResponse(JSON.stringify(result));
 // }
 
+
+// import { NextRequest, NextResponse } from "next/server";
+// import { Kysely, Generated, ColumnType, Selectable, Insertable, Updateable } from "kysely";
+// import { NeonDialect } from "kysely-neon";
+
+// interface playing_with_neon_Table {
+//   id: Generated<number>
+//   name: string
+//   value: number
+// }
+
+// interface Database {
+//   playing_with_neon: playing_with_neon_Table
+// }
+
+// export async function GET(request: NextRequest) {
+//   const db = new Kysely<Database>({
+//     dialect: new NeonDialect({
+//       connectionString: process.env.NEON_DATABASE_URL!,
+//     }),
+//   });
+
+//   const result = await db
+//     .selectFrom("playing_with_neon")
+//     .selectAll()
+//     .execute();
+
+//   console.log("backend result", result);
+//   return new NextResponse(JSON.stringify(result));
+
+// }
+
 import { NextRequest, NextResponse } from "next/server";
 
-import { Kysely, Generated, ColumnType, Selectable, Insertable, Updateable } from "kysely";
+import { Pool } from "pg";
 
-import { NeonDialect } from "kysely-neon";
+import { Kysely, PostgresDialect, Generated, ColumnType, Selectable, Insertable, Updateable } from "kysely";
 
 interface playing_with_neon_Table {
   id: Generated<number>
@@ -28,8 +60,11 @@ interface Database {
 
 export async function GET(request: NextRequest) {
   const db = new Kysely<Database>({
-    dialect: new NeonDialect({
-      connectionString: process.env.NEON_DATABASE_URL!,
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        ssl: true,
+        connectionString: process.env.NEON_DATABASE_URL!,
+      }),
     }),
   });
 
@@ -40,5 +75,5 @@ export async function GET(request: NextRequest) {
 
   console.log("backend result", result);
   return new NextResponse(JSON.stringify(result));
-
 }
+
